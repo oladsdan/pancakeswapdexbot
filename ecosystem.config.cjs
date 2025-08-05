ec0f230a997957723b13e8e775881f1cc2909dd0
unknown <kalloello@gmail.com>
2025-08-05 06:04:53 +0100

    ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 // ecosystem.config.js
 module.exports = {
   apps : [{
-    name   : "pancakeswapsignal",
+    name   : "Dexbotsignal_contracts",
     script : "./index.js",
     instances: 1, // Or 'max' for all available CPU cores
     autorestart: true,
@@ -9,12 +9,13 @@ module.exports = {
     max_memory_restart: '4G', // Restart if memory exceeds 1GB
     env: {
       NODE_ENV: "production",
-      PORT: 3020, // Or whatever port is in your config/default.json
-      MONGODB_URI: "mongodb://localhost:27017/secure_bot", // <-- REPLACE THIS
-      SUBGRAPH_API_KEY: "d389ab06d04a5de3f0115d9a4da59fe8",     // <-- REPLACE THIS
-      FRONTEND_VERCEL_URL: "https://pancakeswapfront.vercel.app",
-      ALCHEMY_API_KEY:"tJBRXMfo8dezfBWD7VlWB",
-      MORALIS_API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjFmY2U2ODRhLTU5YzAtNDUxOS04ZmM0LWNjOGZmM2RiY2EyZiIsIm9yZ0lkIjoiNDU5MDcwIiwidXNlcklkIjoiNDcyMzAwIiwidHlwZUlkIjoiNGU3OTdjMDAtYWYwYy00NjZhLTllYmUtY2E0NWZiNDAxMTcyIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NTIzOTMzNzksImV4cCI6NDkwODE1MzM3OX0.TVMkSduBlKW1jwDsNN27QKgj0gURVCBV97qtjgL1yvc" 
+      // MONGODB_URI: "mongodb://localhost:27017/secure_bot", // <-- REPLACE THIS
+      BSC_RPC_URL : "https://bnb-mainnet.g.alchemy.com/v2/tJBRXMfo8dezfBWD7VlWB",
+      BOT_CONTRACT : "0xa257b7cc03b962888c9812611fbcb843dd274477",
+      PORT : 5001,
+      BSC_RPC_SOC : "wss://bnb-mainnet.g.alchemy.com/v2/tJBRXMfo8dezfBWD7VlWB",
+      MONGO_URI : "mongodb+srv://kalloello:pBjNQ91oft7yxweg@cluster0.80bue4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
     }
   }]
 };
